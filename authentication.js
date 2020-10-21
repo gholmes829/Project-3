2632493bcabf2367cde2479d7702a6a980f6c1dc
Coldparasite <lukespringett@gmail.com>
2020-10-21 16:51:04 -0500

    Updating Authentication

--- authentication.js
+++ authentication.js
@@ -1,17 +1,26 @@
-
-var spotifyApi = new SpotifyWebApi();
-
-spotifyApi.setAccessToken('BQCJzJ5jT30uDzMg9qIgTJsRSJG0B5z1el6LMex0hpxvbbZlUuK7w4ntsCR5uhy7pq4p_5Aw5QZh9IGvhPfwZ3CSB6qz7bXD7o6AxQ9PaNItDrPxPYKxsYQVjDG13s72yokUfQSoGm0ZWiUwAyT8P_MD2zldjtmkruyarGdP8MbUJyMefjK2l9YL-Vdn');
-
 /**
-This is an example of the JMperez spotify web api
+console.log("Initialized");
+var original_location = window.location.href;
+var client_id = '9586133394724f65b7fc986b34fa1a2c';
+var scopes = 'user-top-read';
+var redirect_uri = 'http://localhost:8888/callback';
+var me = null;
+
+function login()
+{
+  console.log(original_location);
+  window.location = `https://accounts.spotify.com/authorize?client_id=${this.client_id}&response_type=token&redirect_uri=${this.redirect_uri}&scope=${this.scopes}&show_dialog=true`;
+
+
+
+  console.log("Login");
+  var popup = window.open(`https://accounts.spotify.com/authorize?client_id=${this.client_id}&response_type=token&redirect_uri=${this.redirect_uri}&scope=${this.scopes}&show_dialog=true`, 'Login with Spotify', 'width=800,height=600');
+  var popup = window.open("/");
+  popup.addEventListener("message",function(){
+    console.log("Popup window recieved message");
+  });
+  popup.postMessage("Sending a message","*");
+  console.log("Completed authentication");
+
+}
 */
-spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
-  function(data) {
-    console.log('Artist albums', data);
-  },
-  function (err) {
-    console.error(err);
-  }
-);
-console.log(spotifyApi.getAccessToken());
